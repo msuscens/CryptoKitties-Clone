8e37a4c758524746035c8d71d5a54248c9c82925
Mark Suscens <mark.suscens@gmail.com>
2021-02-25 16:09:05 +0000

    My solution to Create Kitty assignment (W8D1)

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 
-Assignment Week 7 Day 5
+Assignment Week 7: Day 5
 
-1. Take your front-end Kitties code (develped through to Week 7 Day3)
+1. Take your front-end Kitties code (that you developed throughout Week 7)
 2. Initialise Truffle
 3. Download and add the IERC721.sol interface file (into contracts folder)
-4. Add Kittycontract that inherits from it.
-5. Implement the IERC721 interface functions into the Kittycontract, with any required state variables etc.
-6. Add a token migration file to deploy the contract 
+4. Add a Kitty contract that inherits from it.
+5. Implement the IERC721 interface functions into the Kitty contract, with any required state variables etc.
+6. Add a token migration file to deploy the contract.
+
+Assignment Week 8: Day 1 - Create new Kitty (Building upon previous assignments - see above)
+1. Write a public createKittyGen0(uint256 _genes) function, that employs a private _createkitty function.
+2. Ensure that createKittyGen0() can only be called by the contract owner.
 
 
 
--- contracts/Kittycontract.sol
+++ contracts/Kittycontract.sol
@@ -2,33 +2,67 @@ pragma solidity 0.5.12;
 
 import "./IERC721.sol";
 import "./Safemath.sol";
+import "./Ownable.sol";
 
-contract KittyContract is IERC721 {
+
+contract KittyContract is IERC721, Ownable{
 
     using SafeMath for uint256;
 
     string private _name;
     string private _symbol;
+    uint64 private _GEN0_CREATION_LIMIT;
 
-    mapping (address => uint256) private _ownersKittyCount;
+    uint256 private _gen0KittieCount;
+
+    event Birth(address owner, uint256 kittenId, uint256 mumId, uint256 dadId, uint256 genes);
 
     struct Kitty {
         uint256 genes;
-        uint256 bithTime;
-        uint128 mumId;
-        uint128 dadId;
-        uint128 generation;
+        uint64 birthTime;
+        uint64 mumId;
+        uint64 dadId;
+        uint64 generation;
     }
     Kitty[] private _kitties;  
 
     mapping (uint256 => address) private _kittyOwners;
+    mapping (address => uint256) private _ownersKittyCount;
 
 
     constructor (string memory name, string memory symbol) public {
         _name = name;
         _symbol = symbol;
+        _GEN0_CREATION_LIMIT = 10;
+        _gen0KittieCount = 0;
     }
 
+    function createKittyGen0(uint256 genes) public onlyOwner {
+        require(_gen0KittieCount<_GEN0_CREATION_LIMIT, "Hit Gen0 creation limit!");
+        _gen0KittieCount = _gen0KittieCount.add(1);
+        _createKitty( 0, 0, 0, genes, msg.sender);
+    }
+
+    function _createKitty(uint256 mumId, uint256 dadId, uint256 generation,
+                          uint256 genes, address owner) private returns (uint256){
+        Kitty memory newKitty = Kitty({
+                                    genes: genes,
+                                    birthTime: uint64(now),
+                                    mumId: uint64(mumId),
+                                    dadId: uint64(dadId),
+                                    generation: uint64(generation)
+                                });
+
+        uint256 newKittenId = (_kitties.push(newKitty)).sub(1);
+
+        emit Birth(owner, newKittenId, mumId, dadId, genes);
+
+        _transfer(address(0), owner, newKittenId);
+
+        return newKittenId;
+    }
+
+    // IERC721 function implementations
 
     function balanceOf(address owner) external view returns (uint256 balance){
         return _ownersKittyCount[owner];
@@ -54,8 +88,8 @@ contract KittyContract is IERC721 {
     function transfer(address to, uint256 tokenId) external{
         // Checks
         require(to != address(0), "Recipient's address is zero");
-        require(to != address(this), "Recipient's address is the contract address");
-        require(_isOwner(msg.sender, tokenId), "Sender is not the owner of this token");
+        require(to != address(this), "Recipient is contract address");
+        require(_isOwner(msg.sender, tokenId), "Sender is not token owner");
 
         // Effects - Transfer token
         _transfer(msg.sender, to, tokenId);
@@ -63,7 +97,7 @@ contract KittyContract is IERC721 {
     }
 
     function _transfer(address from, address to, uint256 tokenId) internal{
-        
+
         if (from != address(0)){
             _ownersKittyCount[from] = _ownersKittyCount[from].sub(1);
         }
